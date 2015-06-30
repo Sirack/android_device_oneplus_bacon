566d82a460775e61580c9ba347b05fe6910747c1
Sirack <sirackh@gmail.com>
2015-06-30 19:17:02 +0300

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -40,3 +40,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=A0001 PRODUCT_NAME=bacon
 ifneq ($(SIGN_BUILD),true)
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=oneplus/bacon/A0001:4.4.2/KVT49L/XNPH25R:user/release-keys PRIVATE_BUILD_DESC="bacon-user 4.4.2 KVT49L XNPH25R release-keys"
 endif
+
+$(call inherit-product, device/asus/flo/better-device-vendor.mk)
